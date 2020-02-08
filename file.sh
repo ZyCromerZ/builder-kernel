679a1de9235fb0df959637944be8d013f97ab9d5
ZyCromerZ <neetroid97@gmail.com>
2020-02-08 12:02:53 +0700

    update test upload again

--- file.sh
+++ file.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 echo "done :p"
-echo "test" > test.md
-rsync -avP -e ssh -o StrictHostKeyChecking=no  "./test.md" $my_host@$upload_to:$link_project
+auth() {
+    ssh -o StrictHostKeyChecking=no $my_host@$upload_to
+}
+send() {
+    touch test.md
+    rsync -avP -e ssh "./test.md" $my_host@$upload_to:$link_project
+}
+auth >/dev/null
+send >/dev/null
