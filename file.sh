babdea344fd51905c873629148d7c7e922f01510
ZyCromerZ <neetroid97@gmail.com>
2020-02-08 11:51:38 +0700

    update test upload

--- file.sh
+++ file.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo "done :p"
 touch test.file
+ssh -o StrictHostKeyChecking=no zycromerz@frs.sourceforge.net
 rsync -avP -e ssh "test.file" zycromerz@frs.sourceforge.net:/home/frs/project/zyc-test/
