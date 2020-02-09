b9bc4289df154ae31e06891e66bbfd86b6c82e2d
ZyCromerZ <neetroid97@gmail.com>
2020-02-09 19:51:08 +0700

    Revert "add builder for P"
    This reverts commit 49c01fc68cc4827d6ebbf724ddd39203a9d77c98.

--- private_eg_q.sh
+++ private_eg_q.sh
@@ -58,3 +58,32 @@ buildKernel "Q71Hz" "" "$chat_id_bot_log"
 customInfo "$chat_id_group_indo" "$branch-Q71Hz dah di build . . ."
 
 curl https://github.com/ZyCromerZ/android_kernel_asus_X01BD/commit/f4145eff14f9b35a426f881132e49c8af6077e87.patch | git am -3
+
+git reset --hard $HeadCommit
+
+GetBranch=$(git rev-parse --abbrev-ref HEAD)
+GetCommit=$(git log --pretty=format:'%h' -1)
+
+HeadCommit=$GetCommit
+
+buildKernel "P" "" "$chat_id_bot_log"
+
+customInfo "$chat_id_group_indo" "$branch-P60Hz dah di build . . ."
+
+buildKernel "P65Hz" "" "$chat_id_bot_log"
+
+customInfo "$chat_id_group_indo" "$branch-P65Hz dah di build . . ."
+
+buildKernel "P67Hz" "" "$chat_id_bot_log"
+
+customInfo "$chat_id_group_indo" "$branch-P67Hz dah di build . . ."
+
+buildKernel "P69Hz" "" "$chat_id_bot_log"
+
+customInfo "$chat_id_group_indo" "$branch-P69Hz dah di build . . ."
+
+buildKernel "P71Hz" "" "$chat_id_bot_log"
+
+customInfo "$chat_id_group_indo" "$branch-P71Hz dah di build . . ."
+
+customInfo "$chat_id_group_indo" "@ZyCromerZ semuanya dah beres,cobain gih . . . ."
