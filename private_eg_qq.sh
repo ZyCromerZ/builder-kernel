d3f557fa180778f77d71b85c24c3b0dba6486f78
ZyCromerZ <neetroid97@gmail.com>
2020-02-13 07:46:11 +0700

    change clang 11 to  https://github.com/Haseo97/Clang-11.0.0.git

--- private_eg_qq.sh
+++ private_eg_qq.sh
@@ -4,16 +4,16 @@ branch="EmptyGlory/20200209/Qq"
 # FolderUpload="QuantumKiller"
 # linkKernel="http://bit.ly/QuantumKiller or http://bit.ly/QK-kernels"
 # curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" -d chat_id="$chat_id_bot_log" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="prepare build kernel from <code>https://github.com/ZyCromerZ/android_kernel_asus_X01BD/tree/$branch</code>"
-git clone --depth=1 https://github.com/ZyCromerZ/android_kernel_asus_X01BD -b $branch  kernel_lolz
+git clone --depth=1 https://github.com/ZyCromerZ/android_kernel_asus_X01BD -b $branch  kernel_artoria
 
-cd kernel_lolz
+cd kernel_artoria
 GetBranch=$(git rev-parse --abbrev-ref HEAD)
 GetCommit=$(git log --pretty=format:'%h' -1)
 HeadCommit=$GetCommit
 echo "getting last commit"
 GetREALlog="$(git log --pretty='format:%C(auto)%h : %s' -1)"
 Getlog="${GetREALlog/\&/"and"}"
-git clone --depth=1 https://github.com/Jprimero15/lolz_clang.git -b clang-11.0.0 Getclang
+git clone --depth=1 https://github.com/Haseo97/Clang-11.0.0.git -b 11.0.0 Getclang
 git clone --depth=1 https://github.com/baalajimaestro/aarch64-maestro-linux-android.git -b 05022020 GetGcc
 git clone --depth=1 https://github.com/ZyCromerZ/AnyKernel3 AnyKernel
 
